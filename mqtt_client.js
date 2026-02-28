de4e44479dd1e3a9d1ba33cdc033cc0648245d19
= <=>
2026-02-28 09:16:31 -0300

    Merge branch 'main' of https://github.com/Megax800/GalliumServer

--- mqtt_client.js
+++ mqtt_client.js
@@ -1,7 +1,11 @@
 const mqtt = require("mqtt");
 const fs = require("fs");
 const readline = require('node:readline');
-const client = mqtt.connect("mqtt://192.168.1.110");
+const client = mqtt.connect("mqtt://192.168.1.105");
+const options = {
+    retain: true, // Set the retain flag to true
+    qos: 1          // Optional: Quality of Service level
+};
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -30,17 +34,18 @@ rl.on('line', (input)=>{
             })
         break;
         case 'publish':
-            rl.question("Message: ", (answer) =>{
-                client.publish(myTopic, answer, (err) =>{
+            rl.question("Message: ", (answer1) =>{
+                rl.question("Topic: ", (answer2) =>{
+                client.publish(answer2, answer1, options , (err) =>{
                   if(err){
                         console.log("Error,", err);
                     }else{
-                        console.log('Message published in', myTopic);
-                        myTopic = answer;
+                        console.log('Message published in', answer2);
                     }
                     rl.prompt();
                 });
             })
+        })
         break;
         case 'exit':
             console.log("Bye");
@@ -54,12 +59,11 @@ rl.on('line', (input)=>{
     }
 })
 
-/*client.on("message", (topic, message) => {
+client.on("message", (topic, message) => {
     // message is a Buffer, convert it to a string
     console.log(`Received message on topic "${topic}": ${message.toString()}`);
-    // End the connection after receiving the message (optional for simple examples)
-    client.end();
-});*/
+    rl.prompt();
+});
 
 client.on("error", (err) => {
     console.error("Connection error: ", err);
